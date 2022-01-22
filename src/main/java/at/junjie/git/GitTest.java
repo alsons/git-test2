b4632d460496b419449765308eee72eb91deb1bb
junjie <junjie>
2022-01-22 16:03:43 +0800

    push commit

--- src/main/java/at/junjie/git/GitTest.java
+++ src/main/java/at/junjie/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test2!");
         System.out.println("hot-fix test3!");
-        System.out.println("hot-fix test4!");
+        System.out.println("push 操作");
 
 
     }
